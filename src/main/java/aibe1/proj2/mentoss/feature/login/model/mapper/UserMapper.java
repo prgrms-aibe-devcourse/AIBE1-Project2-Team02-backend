49124cad946028acf37098d019e2fb65c1374ad1
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-05-02 03:15:18 +0900

    Feat: UserMapper 인터페이스 구현

---
+++ src/main/java/aibe1/proj2/mentoss/feature/login/model/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package aibe1.proj2.mentoss.feature.login.model.mapper;
+
+import aibe1.proj2.mentoss.feature.login.model.entity.User;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Mapper
+@Repository
+public interface UserMapper {
+
+    @Select("SELECT * FROM app_user WHERE provider = #{provider} AND provider_id = #{providerId}")
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
+
+    @Select("SELECT * FROM app_user WHERE email = #{email}")
+    Optional<User> findByEmail(String email);
+
+    @Insert("INSERT INTO app_user (region_code, provider, provider_id, email, nickname, profile_image, role, status, report_count, is_deleted, created_at, updated_at) " +
+            "VALUES (#{regionCode}, #{provider}, #{providerId}, #{email}, #{nickname}, #{profileImage}, #{role}, #{status}, #{reportCount}, #{isDeleted}, NOW(), NOW())")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int save(User user);
+
+    @Update("UPDATE app_user SET " +
+            "nickname = #{nickname}, " +
+            "region_code = #{regionCode}, " +
+            "age = #{age}, " +
+            "sex = #{sex}, " +
+            "mbti = #{mbti}, " +
+            "updated_at = NOW() " +
+            "WHERE user_id = #{userId}")
+    int update(User user);
+
+}
