d8f563d5b5ba526b0b3e992f7ec08fbe741d807c
dldsudqlsdlqslek <tina4686@gmail.com>
2025-05-08 14:06:31 +0900

    Chore: Test 컨트롤러 Tag 변경

--- src/main/java/aibe1/proj2/mentoss/feature/testController/rest/CorsTestController.java
+++ src/main/java/aibe1/proj2/mentoss/feature/testController/rest/CorsTestController.java
@@ -1,6 +1,7 @@
 package aibe1.proj2.mentoss.feature.testController.rest;
 
 import aibe1.proj2.mentoss.global.dto.ApiResponseFormat;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * CORS 테스트용 공개 API
  */
 @RestController
+@Tag(name = "TEST - Cors 테스트 API", description = "Cors 설정 테스트용 TEST API")
 public class CorsTestController {
 
     @GetMapping("/api/test/hello")
