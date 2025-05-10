49c8580b3cac8a1189907fa85e6abe32c553da2b
dldsudqlsdlqslek <tina4686@gmail.com>
2025-05-10 20:41:14 +0900

    Refactor: 리뷰생성 dto 수정

--- src/main/java/aibe1/proj2/mentoss/feature/review/model/dto/CreateReviewRequestDto.java
+++ src/main/java/aibe1/proj2/mentoss/feature/review/model/dto/CreateReviewRequestDto.java
@@ -3,7 +3,6 @@ package aibe1.proj2.mentoss.feature.review.model.dto;
 public record CreateReviewRequestDto(
         Long lectureId,
         Long mentorId,
-        Long writerId,
         String content,
         Long rating) {
 }
