a674f776aea3fbc572a67cfde7ae4d97e07f9d24
1000hyehyang <ducogus12@gmail.com>
2025-05-12 10:43:49 +0900

    fix; Tiptap 호환성 개선
    - Tiptap 에디터의 하이라이트 기능(mark 태그 + background-color)을 완전 지원하도록 XssSanitizer 수정
    - <mark style="background-color: ..."> 및 data-color 속성 허용
    - 기존 span, img, heading 태그 지원 유지
    - <script>, onclick 등 위험 요소는 여전히 제거됨

--- src/main/java/aibe1/proj2/mentoss/global/util/XssSanitizer.java
+++ src/main/java/aibe1/proj2/mentoss/global/util/XssSanitizer.java
@@ -1,32 +1,43 @@
 package aibe1.proj2.mentoss.global.util;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Safelist;
+
 /**
  * XSS 공격을 방지하기 위한 문자열 정화 유틸리티 클래스
+ * Tiptap 에디터에서 생성된 HTML을 보존하면서도,
+ * <script> 등 위험 요소는 제거합니다.
  */
 public class XssSanitizer {
 
     /**
      * 입력된 문자열에서 XSS 공격 가능성이 있는 HTML 태그와 스크립트를 제거합니다.
-     * 
+     * Tiptap에서 사용하는 태그(mark, span, img 등) 및 스타일 속성(background-color 등)은 허용합니다.
+     *
      * @param input 정화할 문자열
-     * @return 정화된 문자열
+     * @return 정화된 문자열 (안전한 HTML)
      */
     public static String sanitize(String input) {
         if (input == null) {
             return null;
         }
 
-        // HTML 태그 제거
-        String sanitized = input.replaceAll("<[^>]*>", "");
+        // Tiptap 호환 Safelist 정의
+        Safelist safelist = Safelist.relaxed()
+                .addTags("span", "img", "mark", "h1", "h2", "h3", "h4")
+                .addAttributes("span", "style")
+                .addAttributes("mark", "style", "data-color") // mark 태그 스타일 및 data-color 허용
+                .addAttributes("p", "style")
+                .addAttributes("img", "src", "alt", "title", "width", "height")
+                .addProtocols("img", "src", "data", "https");
 
-        // 특수 문자 이스케이프 처리
-        sanitized = sanitized.replace("&", "&amp;")
-                            .replace("<", "&lt;")
-                            .replace(">", "&gt;")
-                            .replace("\"", "&quot;")
-                            .replace("'", "&#x27;")
-                            .replace("/", "&#x2F;");
+        // Cleaner 사용
+        Document dirty = Jsoup.parseBodyFragment(input, "");
+        Cleaner cleaner = new Cleaner(safelist);
+        Document clean = cleaner.clean(dirty);
 
-        return sanitized;
+        return clean.body().html();
     }
 }
